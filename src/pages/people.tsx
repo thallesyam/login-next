e2a2a0b4bafaf686a8358eb2cdb9f1aac1ff1bae
Thalles Ian <thallesyam@users.=.github.com>
2021-01-14 19:02:58 -0300

    estructure people page

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -1,54 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { NextPageContext } from 'next'
 import { redirectGet } from '../utils/redirectGet'
 import Link from 'next/link'
 import { Container } from '../styles/pages/People'
 
-export default function People({ people }: any): JSX.Element {
-  const [auth, setAuthenticated] = useState<boolean>(false)
-
-  if (JSON.stringify(people)) {
-    setAuthenticated(true)
-  }
-
+export default function People(): JSX.Element {
   return (
     <>
       <Container>
-        {auth && (
-          <>
-            <h1>Página Privada</h1>
-            <p>Você está autenticado</p>
-            <Link href="/signin">
-              <a>Sign in</a>
-            </Link>
-            <br />
-            <Link href="/signup">
-              <a>Sign up</a>
-            </Link>
-            <br />
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-            <br />
-          </>
-        )}
-        {!auth && (
-          <>
-            <h1>Você não está authenticado e não pode acessar o conteudo</h1>
-            <Link href="/signin">
-              <a>Sign in</a>
-            </Link>
-            <br />
-            <Link href="/signup">
-              <a>Sign up</a>
-            </Link>
-            <br />
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-            <br />
-          </>
-        )}
+        <>
+          <h1>Página Privada</h1>
+          <p>Você está authenticado</p>
+          <Link href="/signin">
+            <a>Sign in</a>
+          </Link>
+          <br />
+          <Link href="/signup">
+            <a>Sign up</a>
+          </Link>
+          <br />
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <br />
+        </>
       </Container>
     </>
   )
