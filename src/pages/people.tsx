259908656ab9595dddef35d992af8b4572416869
Thalles Ian <thallesyam@users.=.github.com>
2021-01-14 01:47:16 -0300

    Links People

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { NextPageContext } from 'next'
 import { redirectGet } from '../utils/redirectGet'
+import Link from 'next/link'
 
 export default function People({ people }: any): JSX.Element {
   return (
     <>
       <h1>Hello people {JSON.stringify(people)}</h1>
+
+      <Link href="/signin">
+        <a>Sign in</a>
+      </Link>
+      <br />
+      <Link href="/signup">
+        <a>Sign in</a>
+        <br />
+      </Link>
+      <Link href="/">
+        <a>Home</a>
+        <br />
+      </Link>
     </>
   )
 }
