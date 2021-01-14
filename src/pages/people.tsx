ca4981fdb9b709b38942f935828c9d47db4483cf
Thalles Ian <thallesyam@users.=.github.com>
2021-01-14 18:58:00 -0300

    people page

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NextPageContext } from 'next'
 import { redirectGet } from '../utils/redirectGet'
 import Link from 'next/link'
 import { Container } from '../styles/pages/People'
 
 export default function People({ people }: any): JSX.Element {
-  console.log(JSON.stringify(people))
+  const [auth, setAuthenticated] = useState<boolean>(false)
+
+  if (JSON.stringify(people)) {
+    setAuthenticated(true)
+  }
+
   return (
     <>
       <Container>
-        <h1>Página Privada</h1>
-        <p>Você está autenticado</p>
-        <Link href="/signin">
-          <a>Sign in</a>
-        </Link>
-        <br />
-        <Link href="/signup">
-          <a>Sign up</a>
-        </Link>
-        <br />
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <br />
+        {auth && (
+          <>
+            <h1>Página Privada</h1>
+            <p>Você está autenticado</p>
+            <Link href="/signin">
+              <a>Sign in</a>
+            </Link>
+            <br />
+            <Link href="/signup">
+              <a>Sign up</a>
+            </Link>
+            <br />
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <br />
+          </>
+        )}
+        {!auth && (
+          <>
+            <h1>Você não está authenticado e não pode acessar o conteudo</h1>
+            <Link href="/signin">
+              <a>Sign in</a>
+            </Link>
+            <br />
+            <Link href="/signup">
+              <a>Sign up</a>
+            </Link>
+            <br />
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <br />
+          </>
+        )}
       </Container>
     </>
   )
